b20580b33488365bac0d3c867de512ee06d1a0ce
Andre Ruffert <a.ruffert@gmail.com>
2011-06-07 16:11:01 +0200

    added $DOMAIN

--- collection.sh
+++ collection.sh
@@ -3,34 +3,35 @@
 #------------------------------------------------------------
 # Define Vars
 #------------------------------------------------------------
+DOMAIN="bnfy.de"
 HASH="k43p01d"
 
 
 #------------------------------------------------------------
 # git checkout
 #------------------------------------------------------------
-GIT_WORK_TREE=/var/www/virtual/bf/$HASH.bnfy.de git checkout -f
+GIT_WORK_TREE=/var/www/virtual/bf/$HASH.$DOMAIN git checkout -f
 
 
 #------------------------------------------------------------
 # Datenbankdump einspielen
 #------------------------------------------------------------
-mysql bf_$HASH </var/www/virtual/bf/$HASH.bnfy.de/db.sql
+mysql bf_$HASH </var/www/virtual/bf/$HASH.$DOMAIN/db.sql
 
 
 #------------------------------------------------------------
 # .htaccess RewriteBase aktivieren
 #------------------------------------------------------------
-sed -i 's/#RewriteBase/RewriteBase/g' /var/www/virtual/bf/$HASH.bnfy.de/.htaccess
+sed -i 's/#RewriteBase/RewriteBase/g' /var/www/virtual/bf/$HASH.$DOMAIN/.htaccess
 
 
 #------------------------------------------------------------
 # Contao: localconfig.php $websitePath anpassen
 #------------------------------------------------------------
-sed -i 's/\/wiesauplast//g' /var/www/virtual/bf/$HASH.bnfy.de/system/config/localconfig.php
+sed -i 's/\/wiesauplast//g' /var/www/virtual/bf/$HASH.$DOMAIN/system/config/localconfig.php
 
 
 #------------------------------------------------------------
 # Verzeichnisschutz hinzufügen
 #------------------------------------------------------------
-echo -e '\n\nAuthUserFile /var/www/virtual/bf/.htpasswd\nAuthGroupFile /dev/null\nAuthName "Restricted Area"\nAuthType Basic\n<Limit GET>\nrequire valid-user\n</Limit>' >> /var/www/virtual/bf/$HASH.bnfy.de/.htaccess
+echo -e '\n\nAuthUserFile /var/www/virtual/bf/.htpasswd\nAuthGroupFile /dev/null\nAuthName "Restricted Area"\nAuthType Basic\n<Limit GET>\nrequire valid-user\n</Limit>' >> /var/www/virtual/bf/$HASH.$DOMAIN/.htaccess
