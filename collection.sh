3018df4981e613df7cce96594431890c341ff151
Andre Ruffert <a.ruffert@gmail.com>
2011-06-07 16:53:22 +0200

    fix vPath beim Verzeichnisschutz

--- collection.sh
+++ collection.sh
@@ -4,35 +4,35 @@
 # Define Vars
 #------------------------------------------------------------
 vPATH="/var/www/virtual/bf/"
-DOMAIN="domain.tld"
-HASH="kxxpxxd"
+DOMAIN="bnfy.de"
+HASH="k43p01d"
 
 
 #------------------------------------------------------------
 # git checkout
 #------------------------------------------------------------
-GIT_WORK_TREE=${vPATH}$HASH.$DOMAIN git checkout -f
+GIT_WORK_TREE=$vPATH$HASH.$DOMAIN git checkout -f
 
 
 #------------------------------------------------------------
 # Datenbankdump einspielen
 #------------------------------------------------------------
-mysql bf_$HASH <${vPATH}$HASH.$DOMAIN/db.sql
+mysql bf_$HASH <$vPATH$HASH.$DOMAIN/db.sql
 
 
 #------------------------------------------------------------
 # .htaccess RewriteBase aktivieren
 #------------------------------------------------------------
-sed -i 's/#RewriteBase/RewriteBase/g' ${vPATH}$HASH.$DOMAIN/.htaccess
+sed -i 's/#RewriteBase/RewriteBase/g' $vPATH$HASH.$DOMAIN/.htaccess
 
 
 #------------------------------------------------------------
 # Contao: localconfig.php $websitePath anpassen
 #------------------------------------------------------------
-sed -i 's/\/$HASH//g' ${vPATH}$HASH.$DOMAIN/system/config/localconfig.php
+sed -i 's/\/$HASH//g' $vPATH$HASH.$DOMAIN/system/config/localconfig.php
 
 
 #------------------------------------------------------------
 # Verzeichnisschutz hinzufügen
 #------------------------------------------------------------
-echo -e '\n\nAuthUserFile ${vPATH}.htpasswd\nAuthGroupFile /dev/null\nAuthName "Restricted Area"\nAuthType Basic\n<Limit GET>\nrequire valid-user\n</Limit>' >> ${vPATH}$HASH.$DOMAIN/.htaccess
+echo -e '\n\nAuthUserFile '$vPATH'.htpasswd\nAuthGroupFile /dev/null\nAuthName "Restricted Area"\nAuthType Basic\n<Limit GET>\nrequire valid-user\n</Limit>' >> $vPATH$HASH.$DOMAIN/.htaccess
